72b71603614a5ad6d66ffac06d341f1ae14ed9c7
Tatiana Botia <tatianabb21@gmail.com>
2021-07-28 15:59:01 -0500

    inicio

---
+++ src/containers/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>jfjf</div>
+  )
+}
+
+export default Home
---
+++ src/containers/Success.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Success = () => {
+  return (
+    <div>jfjf</div>
+  )
+}
+
+export default Success
